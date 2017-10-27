fd7ace6760be9ee38c84c62aaf44c4082e5b6781
nagmidde <nagmidde@gmail.com>
2017-10-27 19:36:40 -0400

    xignite url

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var request = require('request');
 
+var delayedquotes = '';
 exports.handler = (event, context, callback) => {
     // TODO implement
     callback(null, 'Hello from Lambda');
+
     
-    request('http://www.google.com', function (error, response, body) {
+    request('https://globalquotes.xignite.com/v3/xGlobalQuotes.json/GetGlobalDelayedQuote?IdentifierType=Symbol&Identifier=TROW&_token=AE4A02E0271A4E77B78B314AEE9A132D', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body) // Print the google web page.
-         }
+         
+            delayedquotes = body;
+        }
     });
 };
